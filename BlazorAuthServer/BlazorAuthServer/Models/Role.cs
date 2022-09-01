944a4f08e49bd1073b39fc40be3bd591397ec1fc
denizatmaca00 <denizatmaca078@gmail.com>
2022-09-01 16:26:16 +0300

    Login-Register

---
+++ BlazorAuthServer/BlazorAuthServer/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAuthServer.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; } 
+        public int UserId { get; set; }
+        public string Administor { get; set; }    
+        public string UserRole { get; set; }    
+        public string CounterRole { get; set; }    
+        
+    }
+}
